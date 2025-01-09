04748efdf7d0c596f8329a42ffb2b58f7938cf75
paulchang <paulchang@qnap.com>
2025-01-09 12:00:11 +0800

    update public path

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,4 +3,9 @@ const common = require("./webpack.common");
 
 module.exports = merge(common, {
   mode: "production",
+  output: {
+    filename: "[name].bundle.js",
+    publicPath: "/image-",
+    path: path.resolve(__dirname, "dist"),
+  },
 });
