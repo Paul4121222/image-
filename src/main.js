153756ffb721b6e13fefa0c69592b5e05fa6ba39
paulchang <paulchang@qnap.com>
2025-01-09 13:16:13 +0800

    redirect to /

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Redirect } from "react-router-dom";
 import SideBarContainer from "./container/SidebarContainer";
 import AlbumContainer from "./container/AlbumContainer";
 import PhotoContainer from "./container/PhotoContainer";
@@ -12,6 +12,7 @@ const Main = () => {
         <Route path="/" element={<PhotoContainer />} />
         <Route path="/album" element={<AlbumContainer />} />
         <Route path="/trash" element={<TrashContainer />} />
+        <Redirect path="/" />
       </Routes>
     </div>
   );
